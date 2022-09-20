b9fe2c9894c66bc637fc98de91869d0be0fcd310
Denise Rosegg <denise1987@gmail.com>
2022-09-20 11:27:48 +0300

    Fixes warning about unused variable.

--- apps/scraper/src/main.ts
+++ apps/scraper/src/main.ts
@@ -1,7 +1,7 @@
 import { getAllLatestMessages } from './scraper';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cron = require('node-cron');
+// const cron = require('node-cron');
 
 // cron.schedule('* * * * *', function () {
 //   console.log('minutely scrape');
